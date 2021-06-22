d82f0796212fde63ba6fcbf3be04f8354eac4f3b
VILLAIN-V01 <80626807+VILLAIN-V01@users.noreply.github.com>
2021-06-22 15:06:19 +0530

    Update fake.py

--- userbot/plugins/fake.py
+++ userbot/plugins/fake.py
@@ -18,7 +18,7 @@ import asyncio
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import ChannelParticipantsAdmins
 from userbot import CMD_HELP
-from mafiabot.utils import admin_cmd
+from heavenbot.utils import admin_cmd
 from userbot.cmdhelp import CmdHelp
 
 @borg.on(admin_cmd(pattern="fake ?(.*)"))
@@ -56,7 +56,7 @@ async def gbun(event):
         # make meself invulnerable cuz why not xD
         if idd == 1212368262:
             await reply_message.reply(
-                "`Wait a second, This is my master!`\n**How dare you threaten to ban my master nigger!**\n\n__Your account has been hacked! Pay 99$ to my master__ [Himanshu](https://t.me/H1M4N5HU0P) __to release your account__😏"
+                "`Wait a second, This is my master!`\n**How dare you threaten to ban my master nigger!**\n\n__Your account has been hacked! Pay 99$ to my master__ [ViLLAiN](https://t.me/ViLLAiN_V01) __to release your account__😏"
             )
         else:
             jnl = (
