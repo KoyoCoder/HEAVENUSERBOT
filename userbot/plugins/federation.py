c30a39f51cccc9bb4abe5802bd0c3604b29a11fa
VILLAIN-V01 <80626807+VILLAIN-V01@users.noreply.github.com>
2021-06-23 17:52:44 +0530

    Update federation.py

--- userbot/plugins/federation.py
+++ userbot/plugins/federation.py
@@ -1,7 +1,7 @@
 # Plugin to show the feds you are banned in.
 # Kangers keep credits
 # By @Surv_ivor
-# fixed some issues by @H1M4N5HU0P
+# fixed some issues by @ViLLAiN_V01
 
 import os
 import asyncio
@@ -9,13 +9,13 @@ from telethon.errors import ChatAdminRequiredError
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.users import GetFullUserRequest
 
-from mafiabot.utils import admin_cmd, edit_or_reply, sudo_cmd
+from heavenbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot import ALIVE_NAME
 from userbot.cmdhelp import CmdHelp
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Mafia User"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Heaven User"
 
-mafia = bot.uid
+heaven = bot.uid
 
 
 bots = "@MissRose_bot"
@@ -313,7 +313,7 @@ EXCLUDE_FED = os.environ.get("EXCLUDE_FED", None)
 async def _(event):
     if event.fwd_from:
         return
-    await event.edit(f"Starting superfban By [{DEFAULTUSER}](tg://user?id={mafia}) 😈")
+    await event.edit(f"Starting superfban By [{DEFAULTUSER}](tg://user?id={heaven}) 😈")
     fedList = []
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
@@ -361,7 +361,7 @@ async def _(event):
             or FBAN == "@HeisenbergTheDanger"
             or FBAN == "@xditya"
             or FBAN == "@Bhomik16"
-            or FBAN == "@H1M4N5HU0P"
+            or FBAN == "@ViLLAiN_V01"
             or FBAN == "@Pacifist_Op"
     
         ):
@@ -424,7 +424,7 @@ async def _(event):
         if len(fedList) == 0:
             await event.edit("Something went wrong.")
             return
-    await event.edit(f"Fbaning in {len(fedList)} feds by [{DEFAULTUSER}](tg://user?id={mafia}) 😈")
+    await event.edit(f"Fbaning in {len(fedList)} feds by [{DEFAULTUSER}](tg://user?id={heaven}) 😈")
     try:
         await event.client.send_message(chat, f"/start")
     except:
@@ -443,9 +443,9 @@ async def _(event):
             continue
         await event.client.send_message(chat, f"/joinfed {fed}")
         await asyncio.sleep(3)
-        await event.client.send_message(chat, f"/fban {FBAN} {REASON} \n\n#SUPERFBAN_BY_MAFIABOT")
+        await event.client.send_message(chat, f"/fban {FBAN} {REASON} \n\n#SUPERFBAN_BY_HEAVENBOT")
         await asyncio.sleep(3)
-    await event.edit(f"SuperFBan Completed. Affected {len(fedList) - exCount} feds by [{DEFAULTUSER}](tg://user?id={mafia}) 😈")
+    await event.edit(f"SuperFBan Completed. Affected {len(fedList) - exCount} feds by [{DEFAULTUSER}](tg://user?id={heaven}) 😈")
 
 
 # By @HeisenbergTheDanger, @its_xditya
@@ -454,7 +454,7 @@ async def _(event):
 async def _(event):
     if event.fwd_from:
         return
-    await event.edit(f"Starting a superunfban by [{DEFAULTUSER}](tg://user?id={mafia}) ⚡")
+    await event.edit(f"Starting a superunfban by [{DEFAULTUSER}](tg://user?id={heaven}) ⚡")
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
         FBAN = previous_message.sender_id
@@ -508,7 +508,7 @@ async def _(event):
         elif In:
             tempFedId += x
 
-    await event.edit(f"UnFbaning in {len(fedList)} feds by [{DEFAULTUSER}](tg://user?id={mafia}) ⚡")
+    await event.edit(f"UnFbaning in {len(fedList)} feds by [{DEFAULTUSER}](tg://user?id={heaven}) ⚡")
     try:
         await event.client.send_message(chat, f"/start")
     except:
@@ -518,9 +518,9 @@ async def _(event):
     for fed in fedList:
         await event.client.send_message(chat, f"/joinfed {fed}")
         await asyncio.sleep(3)
-        await event.client.send_message(chat, f"/unfban {FBAN} \n\n#SUPERUNFBAN_BY_MAFIABOT")
+        await event.client.send_message(chat, f"/unfban {FBAN} \n\n#SUPERUNFBAN_BY_HEAVENBOT")
         await asyncio.sleep(3)
-    await event.edit(f"SuperUnFBan Completed. Affected {len(fedList)} feds by [{DEFAULTUSER}](tg://user?id={mafia}) ⚡")
+    await event.edit(f"SuperUnFBan Completed. Affected {len(fedList)} feds by [{DEFAULTUSER}](tg://user?id={heaven}) ⚡")
     
 CmdHelp("federation").add_command(
   "fstat", "<username/user id>", "to get fban info"
