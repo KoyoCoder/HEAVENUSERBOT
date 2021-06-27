9c2a8bec22381901ac882c505028e14c7c99f720
VILLAIN-V01 <80626807+VILLAIN-V01@users.noreply.github.com>
2021-06-27 18:15:25 +0530

    Update rename.py

--- userbot/plugins/rename.py
+++ userbot/plugins/rename.py
@@ -184,7 +184,7 @@ async def _(event):
                     event.chat_id,
                     downloaded_file_name,
                     thumb=thumb,
-                    caption="reuploaded by [MafiaBot](https://t.me/MafiaBot_Chit_Chat)",
+                    caption="reuploaded by [HeavenBot](https://t.me/HeavenBot_Support)",
                     force_document=False,
                     allow_cache=False,
                     reply_to=event.message.id,
