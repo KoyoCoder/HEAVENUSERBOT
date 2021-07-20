c61d44d146f480c11e74821a3e69ab19903ff912
VILLAIN-V01 <80626807+VILLAIN-V01@users.noreply.github.com>
2021-07-20 14:59:48 +0530

    Update logo.py

--- userbot/plugins/logo.py
+++ userbot/plugins/logo.py
@@ -23,7 +23,7 @@ from telethon.tl.types import InputMessagesFilterPhotos
 from . import *
 
 
-@heaven_cmd(pattern="logo ?(.*)")
+@heavenbot_cmd(pattern="logo ?(.*)")
 async def logo_gen(event):
     xx = await eor(event, get_string("com_1"))
     name = event.pattern_match.group(1)
